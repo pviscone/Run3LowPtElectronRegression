4a85c14a72bb3558972651d219dc2ad277fe2b64
Piero Viscone <pieroviscone@gmail.com>
2025-10-13 15:16:24 +0200

    added plotting scripts

---
+++ step1_plots/plot_distributions.py
@@ -0,0 +1,17 @@
+#%%
+import sys
+sys.path.append("..")
+from utils.plots import plot_distributions
+from utils.features import features_eb, features_ee
+import pandas as pd
+
+df = pd.read_pickle("../data/full_data_splitted_w.pkl")
+
+#%%
+plot_distributions(df[df["LPEle_isEB"]==1], features=features_eb+["LPEle_target"], log=True, weight="LPEle_w", savefolder="plots/features/eb_w")
+plot_distributions(df[df["LPEle_isEB"]==1], features=features_eb+["LPEle_target", "LPEle_w"], log=True, savefolder="plots/features/eb")
+
+#%%
+plot_distributions(df[df["LPEle_isEB"]==0], features=features_ee+["LPEle_target"], log=True, weight="LPEle_w", savefolder="plots/features/ee_w")
+plot_distributions(df[df["LPEle_isEB"]==0], features=features_ee+["LPEle_target", "LPEle_w"], log=True, savefolder="plots/features/ee")
+# %%
