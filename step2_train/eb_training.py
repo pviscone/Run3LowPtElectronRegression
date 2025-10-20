dd7ee6ad9ab77d4a17da9fb4b195071f1512f6a5
Piero Viscone <pieroviscone@gmail.com>
2025-10-20 20:23:39 +0200

    Updated networks

--- step2_train/eb_training.py
+++ step2_train/eb_training.py
@@ -9,18 +9,22 @@ import sys
 import numpy as np
 
 sys.path.append("..")
-from utils.features import features_eb
+from utils.features import (
+    tk_features,
+    caloEB_features,
+    common_features,
+    combined_features,
+)
 from utils.plots import response_resolution
 
 hep.style.use("CMS")
 
-os.makedirs("plots_eb", exist_ok=True)
-
 df = pd.read_pickle("../data/full_data_splitted_w.pkl")
 df = df[df["LPEle_isEB"] == 1]
-df = df[df["LPEle_caloTarget"] < 5]
-df = df[df["LPEle_tkTarget"] < 5]
+# df = df[df["LPEle_caloTarget"] < 5]
+# df = df[df["LPEle_tkTarget"] < 5]
 
+features_eb = common_features + combined_features + tk_features + caloEB_features
 train_data = df[features_eb].to_numpy()
 targets = df[["LPEle_caloTarget", "LPEle_tkTarget"]].to_numpy()
 w = df["LPEle_w"].to_numpy()
@@ -29,37 +33,49 @@ df_train, df_test, X_train, X_val, Y_train, Y_val, w, _ = train_test_split(
     df, train_data, targets, w, test_size=0.2, random_state=666
 )
 
+
 # %%
 def callback(model, savefolder, nepochs):
     os.makedirs(f"{savefolder}/plots_eb", exist_ok=True)
     train_loss, val_loss = model.get_loss()
     fig, ax = plt.subplots()
     ax.plot(train_loss, label="train")
-    if len(val_loss)>0:
+    if len(val_loss) > 0:
         ax.plot(val_loss, label="val")
     ax.set_xlabel("Epoch")
     ax.set_ylabel("Loss")
     ax.legend()
     fig.savefig(f"{savefolder}/plots_eb/loss_eb.pdf")
 
-
     df_test["LPEle_ERatio"] = df_test["LPEle_energy"] / df_test["LPEle_Gen_p"]
     df_test["LPEle_pRatio"] = df_test["LPEle_Tk_p"] / df_test["LPEle_Gen_p"]
-    df_test["LPEle_combPRatio"] = df_test["LPEle_pt"] * np.cosh(df_test["LPEle_eta"])/ df_test["LPEle_Gen_p"]
+    df_test["LPEle_combPRatio"] = (
+        df_test["LPEle_pt"] * np.cosh(df_test["LPEle_eta"]) / df_test["LPEle_Gen_p"]
+    )
 
     df_test["LPEle_ECorrRatio"] = (
-        model.mu(df_test[features_eb].to_numpy())[:,0] * df_test["LPEle_ERatio"]
+        model.mu(df_test[features_eb].to_numpy())[:, 0] * df_test["LPEle_ERatio"]
     )
     df_test["LPEle_pCorrRatio"] = (
-        model.mu(df_test[features_eb].to_numpy())[:,1] * df_test["LPEle_pRatio"]
+        model.mu(df_test[features_eb].to_numpy())[:, 1] * df_test["LPEle_pRatio"]
     )
 
-    blue_mu, _ = model.BLUE(df_test[features_eb].to_numpy(), df_test["LPEle_energy"].to_numpy(), df_test["LPEle_Tk_p"].to_numpy())
+    blue_mu, _ = model.BLUE(
+        df_test[features_eb].to_numpy(),
+        df_test["LPEle_energy"].to_numpy(),
+        df_test["LPEle_Tk_p"].to_numpy(),
+    )
     df_test["LPEle_combPCorrRatio"] = blue_mu / df_test["LPEle_Gen_p"]
 
-    pratio_dict_calo = {"No Regression": "LPEle_ERatio", "Regressed": "LPEle_ECorrRatio"}
+    pratio_dict_calo = {
+        "No Regression": "LPEle_ERatio",
+        "Regressed": "LPEle_ECorrRatio",
+    }
     pratio_dict_tk = {"No Regression": "LPEle_pRatio", "Regressed": "LPEle_pCorrRatio"}
-    pratio_dict_comb = {"No Regression": "LPEle_combPRatio", "Regressed": "LPEle_combPCorrRatio"}
+    pratio_dict_comb = {
+        "No Regression": "LPEle_combPRatio",
+        "Regressed": "LPEle_combPCorrRatio",
+    }
 
     response_resolution(
         df_test,
@@ -83,7 +99,7 @@ def callback(model, savefolder, nepochs):
         savefolder=f"{savefolder}/plots_eb/tk",
     )
 
-    #TODO FIX BLUE
+    # TODO FIX BLUE
     response_resolution(
         df_test,
         pratio_dict_comb,
@@ -95,8 +111,7 @@ def callback(model, savefolder, nepochs):
         savefolder=f"{savefolder}/plots_eb/comb",
     )
 
-    #TODO implement calibration and correlation curves
-
+    # TODO implement calibration and correlation curves
 
 
 model = DoubleMVENet(
@@ -107,10 +122,34 @@ model = DoubleMVENet(
     n_hidden_mean2=[512, 256, 128, 64, 32],
     n_hidden_var2=[512, 256, 128, 64, 32],
     n_hidden_rho=[512, 256, 128, 64, 32],
-    loss="BivariateL2",
+    loss="MahalanobisL1",
 )
 
-model.normalize(X_train, Y_train)
+model.normalize(X_train)
+model.init_rho()
+model.freeze([".*rho.*"])
+model.train_model(
+    X_train,
+    Y_train,
+    sample_weight=w,
+    X_val=X_val,
+    Y_val=Y_val,
+    batch_size=10280,
+    learn_rate=1e-6,
+    n_epochs=50,
+    reg_var1=1e-3,
+    reg_mean1=1e-4,
+    reg_var2=1e-3,
+    reg_mean2=1e-4,
+    reg_rho=1e-2,
+    max_norm=None,
+    checkpoint=50,
+    callback=callback,
+    callback_every=50,
+    savefolder="model_MahalanobisL1",
+)
+# %%
+model.unfreeze()
 model.train_model(
     X_train,
     Y_train,
@@ -126,16 +165,14 @@ model.train_model(
     reg_mean2=1e-4,
     reg_rho=1e-2,
     max_norm=None,
-    checkpoint=10,
+    checkpoint=50,
     callback=callback,
-    callback_every=10,
-    savefolder="modelL2_eb",
+    callback_every=50,
+    savefolder="model_MahalanobisL1",
 )
-
-#%%
-#model.save("model_eb")
+# %%
+# model.save("model_eb")
 # %%
 
 # %%
-#model = DoubleMVENet.load("model_eb")
-
+# model = DoubleMVENet.load("model_eb")
--- step2_train/eb_training_calo.py
+++ step2_train/eb_training_calo.py
@@ -5,41 +5,108 @@ from MVENet.SingleMVENet import SingleMVENet
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import mplhep as hep
+import numpy as np
 import os
 import sys
 
 sys.path.append("..")
-from utils.features import features_eb
+from utils.features import caloEB_features, common_features, to_log
+from utils.plots import response_resolution, plot_calibration_curve
 
 hep.style.use("CMS")
 
-os.makedirs("plots_eb", exist_ok=True)
-
 df = pd.read_pickle("../data/full_data_splitted_w.pkl")
 df = df[df["LPEle_isEB"] == 1]
-# df = df[df["LPEle_target"] < 4]
-# df = df[df["LPEle_target"] > 0.25]
-
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=666)
-
-X_train = df_train[features_eb].to_numpy()
-Y_train = df_train["LPEle_caloTarget"].to_numpy()
-w = df_train["LPEle_w"].to_numpy()
-X_val = df_test[features_eb].to_numpy()
-Y_val = df_test["LPEle_caloTarget"].to_numpy()
+df = df[df["LPEle_caloTarget"] < 10]
+# df = df[df["LPEle_tkTarget"] < 5]
 
 
+for feature in to_log:
+    if feature.startswith("-"):
+        df[feature] = np.log(1e-8 - df[feature[1:]].values)
+    else:
+        df[feature] = np.log(1e-8 + df[feature].values)
+
+
+features_eb = common_features + caloEB_features
+train_data = df[features_eb].to_numpy()
+targets = df["LPEle_caloTarget"].to_numpy()
+w = df["LPEle_w"].to_numpy()
+
+df_train, df_test, X_train, X_val, Y_train, Y_val, w, _ = train_test_split(
+    df, train_data, targets, w, test_size=0.2, random_state=666
+)
+
+"""
+mask = df_train["LPEle_caloTarget"].values < 10
+df_train = df_train[mask]
+X_train = X_train[mask]
+Y_train = Y_train[mask]
+w = w[mask]
+"""
 # %%
+def callback(model, savefolder, nepochs):
+    os.makedirs(f"{savefolder}/plots", exist_ok=True)
+    train_loss, val_loss = model.get_loss()
+    fig, ax = plt.subplots()
+    ax.plot(train_loss, label="train")
+    if len(val_loss) > 0:
+        ax.plot(val_loss, label="val")
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("Loss")
+    ax.legend()
+    fig.savefig(f"{savefolder}/plots/loss_eb.pdf")
+
+    df_test["LPEle_ERatio"] = df_test["LPEle_energy"] / df_test["LPEle_Gen_p"]
+    mu, sigma = model.f(df_test[features_eb].to_numpy())
+    df_test["LPEle_corrFact"] = mu
+    df_test["LPEle_sigma"] = sigma
+
+    df_test["LPEle_ECorrRatio"] = df_test["LPEle_corrFact"] * df_test["LPEle_ERatio"]
+
+    pratio_dict_calo = {
+        "No Regression": "LPEle_ERatio",
+        "Regressed": "LPEle_ECorrRatio",
+    }
+
+    response_resolution(
+        df_test,
+        pratio_dict_calo,
+        "LPEle_Gen_p",
+        "LPEle_Gen_eta",
+        plot_distributions=False,
+        eta_bins=[0, 0.25, 0.5, 0.75, 1, 1.25, 1.47],
+        lab="E_{\\text{calo}}",
+        savefolder=f"{savefolder}/plots/calo",
+    )
+
+    plot_calibration_curve(
+        df_test,
+        "LPEle_caloTarget",
+        "LPEle_corrFact",
+        "LPEle_sigma",
+        sigma_bins=np.arange(0, 2, 0.05),
+        genp_bins=[0, 40, 80, 100, 200, 400, 600],
+        eta_bins=[0, 0.8, 1.44],
+        savefolder=f"{savefolder}/plots/calo",
+        metric="L1",
+    )
+
+
 
 model = SingleMVENet(
     input_shape=len(features_eb),
-    n_hidden_common=[512, 256, 128],
-    n_hidden_mean=[128, 128, 64, 32],
-    n_hidden_var=[128, 128, 64, 32],
+    n_hidden_common=[],
+    n_hidden_mean1=[512, 256, 128, 64, 32],
+    n_hidden_var1=[512, 256, 128, 64, 32],
+    dropout_input=0.,
+    dropout_mean1=0.,
+    dropout_var1=0.,
     loss="L1Loss",
 )
 
-model.normalize(X_train, Y_train)
+model.normalize(X_train)
+model.freeze([".*var.*"])
 model.train_model(
     X_train,
     Y_train,
@@ -47,48 +114,37 @@ model.train_model(
     X_val=X_val,
     Y_val=Y_val,
     batch_size=10280,
-    learn_rate=1e-4,
-    n_epochs=80,
-    reg_var=1e-3,
-    reg_mean=1e-4,
-    verbose=True,
+    learn_rate=1e-3,
+    n_epochs=60,
+    reg_var1=0.1,
+    reg_mean1=0.  ,
+    max_norm=None,
+    checkpoint=30,
+    callback=callback,
+    callback_every=30,
+    savefolder="model_caloEBL1",
 )
-
-
-model.save("model_eb")
-# %%
-fig, ax = plt.subplots()
-ax.plot(model.train_loss.loss_total, label="train")
-if hasattr(model, "val_loss"):
-    ax.plot(model.val_loss.loss_total, label="val")
-ax.set_xlabel("Epoch")
-ax.set_ylabel("Loss")
-ax.legend()
-fig.savefig("plots_eb/loss_eb.pdf")
-# %%
-model = SingleMVENet.load("model_eb")
-
-# %%
-import sys
-
-sys.path.append("..")
-from utils.plots import response_resolution
-import pandas as pd
-
-df_test["LPEle_pRatio"] = df_test["LPEle_energy"] / df_test["LPEle_Gen_p"]
-df_test["LPEle_pCorrRatio"] = (
-    model.f(df_test[features_eb].to_numpy()) * df_test["LPEle_pRatio"]
-)
-pratio_dict = {"No Regression": "LPEle_pRatio", "Regressed": "LPEle_pCorrRatio"}
-
-
-response_resolution(
-    df_test,
-    pratio_dict,
-    "LPEle_Gen_p",
-    "LPEle_Gen_eta",
-    plot_distributions=False,
-    eta_bins=[0, 0.25, 0.5, 0.75, 1, 1.25, 1.47],
-    savefolder="plots_eb",
+model.unfreeze()
+model.train_model(
+    X_train,
+    Y_train,
+    sample_weight=w,
+    X_val=X_val,
+    Y_val=Y_val,
+    batch_size=10280,
+    learn_rate=8e-4,
+    n_epochs=1000,
+    reg_var1=0.05,
+    reg_mean1=0.  ,
+    max_norm=None,
+    checkpoint=50,
+    callback=callback,
+    callback_every=50,
+    savefolder="model_caloEBL1",
 )
 # %%
+# model.save("model_eb")
+# %%
+
+# %%
+# model = SingleMVENet.load("model_eb")
